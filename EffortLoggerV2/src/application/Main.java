6ff9d591e514cad6b2d33796f0bfb712224ee0be
rswart0604 <rswart0604@gmail.com>
2023-10-24 15:52:27 -0700

    second commit

---
+++ EffortLoggerV2/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application {
+	public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
